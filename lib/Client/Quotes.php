67fe941e2668d51fb88eb0ab80859938dcb0e2bb
alexeyostapets <oleksii.ostapets@gmail.com>
2013-07-09 17:05:11 +0300

    columns

--- lib/Client/Quotes.php
+++ lib/Client/Quotes.php
@@ -8,6 +8,7 @@ class Client_Quotes extends View {
         $this->api->stickyGET($this->name);
 
         $v=$this->add('View')->setClass('span6 left');
+        
         $v->add('H4')->set('1. Quotes requested');
         $this->quotes=$grid=$v->add('Grid');
         $m=$grid->setModel('Quote',array('project','user','name'));
@@ -34,8 +35,24 @@ class Client_Quotes extends View {
                 ->execute();
         }
         
+        
         $v=$this->add('View')->setClass('span6 right');
-        $v->add('H4')->set('3. Quotes estimated (developer returned)');
+        
+        $v->add('H4')->set('3. Quotes estimate requested (sent to developers for estimation)');
+        $this->quotes=$cr=$v->add('CRUD',array('allow_add'=>false,'allow_edit'=>false,'allow_del'=>false));
+        $m=$cr->setModel('Quote',array('project','user','name'));
+        $m->addCondition('status','estimate_needed');
+        $m->addCondition('client_id',$this->api->auth->model['client_id']);
+            if($cr->grid){
+        	$cr->grid->addColumn('button','details');
+        }
+        if($_GET['details']){
+            $this->js()->univ()->redirect($this->api->url('/client/rfq/view',
+                        array('quote_id'=>$_GET['details'])))
+                ->execute();
+        }
+
+        $v->add('H4')->set('4. Quotes estimated (developer returned)');
         $this->quotes=$cr=$v->add('CRUD',array('allow_add'=>false,'allow_edit'=>false,'allow_del'=>false));
         $m=$cr->setModel('Quote',array('project','user','name','estimated'));
         $m->addCondition('status','estimated');
@@ -49,18 +66,5 @@ class Client_Quotes extends View {
                 ->execute();
         }
                 
-        $v->add('H4')->set('4. Quotes estimate requested (sent to developers for estimation)');
-        $this->quotes=$cr=$v->add('CRUD',array('allow_add'=>false,'allow_edit'=>false,'allow_del'=>false));
-        $m=$cr->setModel('Quote',array('project','user','name'));
-        $m->addCondition('status','estimate_needed');
-        $m->addCondition('client_id',$this->api->auth->model['client_id']);
-            if($cr->grid){
-        	$cr->grid->addColumn('button','details');
-        }
-        if($_GET['details']){
-            $this->js()->univ()->redirect($this->api->url('/client/rfq/view',
-                        array('quote_id'=>$_GET['details'])))
-                ->execute();
-        }
     }
 }
