8aa3550240ca824ea1ccac20a0310482658a1f55
alexeyostapets <oleksii.ostapets@gmail.com>
2013-07-12 11:42:17 +0300

    db fix

--- doc/dbupdates/col-001-18.sql
+++ doc/dbupdates/col-001-18.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `attach` ADD COLUMN `task_id` INT(11) NULL  AFTER `updated_dts` , 
   ADD CONSTRAINT `fk_attach_task1`
   FOREIGN KEY (`task_id` )
-  REFERENCES `col`.`task` (`id` )
+  REFERENCES `task` (`id` )
   ON DELETE NO ACTION
   ON UPDATE NO ACTION
 , ADD INDEX `fk_attach_task1` (`task_id` ASC) ;
