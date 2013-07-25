be3597e4c43af03968866d2a0a0ff9fb919d0f15
alexeyostapets <oleksii.ostapets@gmail.com>
2013-07-25 15:56:47 +0300

    show pictures for requirement's grid

--- lib/Grid.php
+++ lib/Grid.php
@@ -2,8 +2,22 @@
 class Grid extends Grid_Advanced {
     function format_download($field){//var_dump($this->current_row['migrated_image']);
     	if ($this->current_row[$field]!=""){
-        	$this->current_row_html[$field] =
-            	'<a target="_blank" href="'. $this->current_row[$field] .'">download</a>';
+    		$name=$this->current_row[$field];
+    		$arr_name=explode('.',$name);
+    		$image_exts=array(
+    				'jpg',
+    				'jpeg',
+    				'png',
+    				'gif',
+    				'bmp',
+    				);
+    		if(in_array($arr_name[count($arr_name)-1],$image_exts)){
+	        	$this->current_row_html[$field] =
+    		        	'<a target="_blank" href="'. $this->current_row[$field] .'"><img width="150" src="'.$this->current_row[$field].'"></a>';
+    		}else{
+	        	$this->current_row_html[$field] =
+    		        	'<a target="_blank" href="'. $this->current_row[$field] .'">download</a>';
+    		}
     	}else{
     		$this->current_row_html[$field] = '';
     	}
