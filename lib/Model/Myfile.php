b304138a8d8a8b702fe7c58bd9421c54391e3c13
alexeyostapets <oleksii.ostapets@gmail.com>
2013-07-25 15:48:09 +0300

    possibility to upload all file types

---
+++ lib/Model/Myfile.php
@@ -0,0 +1,4 @@
+<?php
+class Model_Myfile extends filestore\Model_File {
+    public $policy_add_new_type=true; // set this to true, will allow to upload all file types
+}
--- lib/Model/Requirement.php
+++ lib/Model/Requirement.php
@@ -8,8 +8,13 @@ class Model_Requirement extends Model_Table {
 		$this->addField('name')->mandatory('required');
 		$this->addField('descr')->type('text');
 		$this->addField('estimate');
-		$this->add('filestore\Field_File','file_id')->setModel('filestore/File');
-
+		//$this->add('filestore\Field_File','file_id')->setModel('filestore/File');
+		
+		$this->add('filestore/Field_File', array(
+				'name'=>'file_id',
+				'use_model'=>'Model_Myfile'
+		));
+		
         $this->addExpression('spent_time')->set(function($m,$q){
             return $q->dsql()
                 ->table('task')
