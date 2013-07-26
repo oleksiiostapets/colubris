1e138abe885e3ca40d48358b172a680958364f2c
alexeyostapets <oleksii.ostapets@gmail.com>
2013-07-26 16:35:24 +0300

    fix for participants

--- lib/Model/Quote/Participant.php
+++ lib/Model/Quote/Participant.php
@@ -4,7 +4,7 @@ class Model_Quote_Participant extends Model_Quote {
     function init(){
         parent::init();
 
-        $participated_in=$this->add('Model_Participant')->tryLoadBy('user_id',$this->api->auth->model['id']);
+        $participated_in=$this->add('Model_Participant')->addCondition('user_id',$this->api->auth->model['id']);
         $projects_ids="";
         foreach($participated_in as $p){
         	if($projects_ids=="") $projects_ids=$p['project_id'];
