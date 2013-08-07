113a51ca1d8b6bde1f69db1752f8e9719372916a
alexeyostapets <oleksii.ostapets@gmail.com>
2013-08-07 12:27:18 +0300

    up for requester

--- page/team/tasks.php
+++ page/team/tasks.php
@@ -8,8 +8,8 @@ class page_team_tasks extends page_tasksfunctions {
     	$this->add('View_TasksCRUD',array(
     			'newtask_link'=>'team/tasks/new',
     			'allow_add'=>false,'allow_edit'=>true,'allow_del'=>true,
-    			'edit_fields'=>array('name','descr_original','priority','status','estimate','assigned_id'),
-    			'show_fields'=>array('name','descr_original','priority','status','estimate','spent_time','assigned'),
+    			'edit_fields'=>array('name','descr_original','priority','status','estimate','requester_id','assigned_id'),
+    			'show_fields'=>array('name','descr_original','priority','status','estimate','spent_time','requester','assigned'),
     			));
     }
     
