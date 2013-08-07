05c1b29eb4f1d88f538a8eedd9a25e6f9a991218
alexeyostapets <oleksii.ostapets@gmail.com>
2013-08-07 18:05:34 +0300

    dashboard

--- page/client/tasks.php
+++ page/client/tasks.php
@@ -9,7 +9,7 @@ class page_client_tasks extends page_tasksfunctions {
     			'newtask_link'=>'client/tasks/new',
     			'allow_add'=>false,'allow_edit'=>true,'allow_del'=>true,
     			'edit_fields'=>array('name','descr_original','priority','status','requester_id','assigned_id'),
-    			'show_fields'=>array('name','descr_original','priority','status','estimate','spent_time','requester','assigned'),
+    			'show_fields'=>array('name','priority','status','estimate','spent_time','requester','assigned'),
     			));    	
     }
         
