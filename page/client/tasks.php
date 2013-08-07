ab68ad02e2fd59dd21f0fad2b2215494e63e5311
alexeyostapets <oleksii.ostapets@gmail.com>
2013-08-07 12:27:18 +0300

    up for requester

--- page/client/tasks.php
+++ page/client/tasks.php
@@ -8,8 +8,8 @@ class page_client_tasks extends page_tasksfunctions {
     	$this->add('View_TasksCRUD',array(
     			'newtask_link'=>'client/tasks/new',
     			'allow_add'=>false,'allow_edit'=>true,'allow_del'=>true,
-    			'edit_fields'=>array('name','descr_original','priority','status','assigned_id'),
-    			'show_fields'=>array('name','descr_original','priority','status','estimate','spent_time','assigned'),
+    			'edit_fields'=>array('name','descr_original','priority','status','requester_id','assigned_id'),
+    			'show_fields'=>array('name','descr_original','priority','status','estimate','spent_time','requester','assigned'),
     			));    	
     }
         
