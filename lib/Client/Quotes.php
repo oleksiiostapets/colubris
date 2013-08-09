691cdaa4c544b72d073a57f18109c738be01460c
alexeyostapets <oleksii.ostapets@gmail.com>
2013-08-09 09:15:11 +0300

    test

--- lib/Client/Quotes.php
+++ lib/Client/Quotes.php
@@ -22,7 +22,7 @@ class Client_Quotes extends View {
         $cr=$this->add('Grid_Quotes');
         $m=$this->add('Model_Quote');
         echo $this->api->auth->model['client_id'];
-        //$m->addCondition('client_id',$this->api->auth->model['client_id']);
+        $m->addCondition('client_id',$this->api->auth->model['client_id']);
         $cr->setModel($m,array('project','user','name','estimated','estimpay','spent_time','rate','currency','durdead','status','client_id'));
         $cr->addFormatter('status','status');
        	$cr->addColumn('button','edit');
