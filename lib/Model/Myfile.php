9572d86e0698e6d146de377d20e4f6a999879a88
vadym <vadym@xxx.(none)>
2013-08-14 22:30:25 +0300

    Tasks :: thumb for image and link for other files in Comments

--- lib/Model/Myfile.php
+++ lib/Model/Myfile.php
@@ -1,4 +1,4 @@
 <?php
-class Model_Myfile extends filestore\Model_File {
+class Model_Myfile extends Model_ImageOrFile {
     public $policy_add_new_type=true; // set this to true, will allow to upload all file types
 }
