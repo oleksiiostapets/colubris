8fbc56aa59a97df52d51598e968097a7e7295a19
vadym <vadym@xxx.(none)>
2013-08-16 11:25:47 +0300

    quotes grid for manager

--- lib/Grid/Quotes.php
+++ lib/Grid/Quotes.php
@@ -1,29 +1,106 @@
 <?php
-class Grid_Quotes extends Grid_Advanced {
+class Grid_Quotes extends Grid {
+    public $posible_actions = array(
+        'requirements'   => array('status'=>'Quotation Requested','name'=>'Requirements',            'get_var'=>'requirements'),
+        'estimation'     => array('status'=>'Quotation Requested','name'=>'Request for estimate',    'get_var'=>'estimation'),
+        'send_to_client' => array('status'=>'Estimated',          'name'=>'Send Quote to the client','get_var'=>'send_to_client'),
+        'approve'        => array('status'=>'Estimated',          'name'=>'Approve Estimation',      'get_var'=>'approve'),
+        'estimate'       => array('status'=>'Estimate Needed',    'name'=>'Estimate',                'get_var'=>'estimate'),
+    );
     function init() {
         parent::init();
+        if (!isset($this->owner->allowed_actions)) {
+            throw $this->exception('What actions are allowed?');
+        }
+
+
+        /* ********************************
+         *
+         *     perform action if allowed
+         *
+         */
+
+        // estimate
+        if( $_GET['estimate'] ) {
+            if ( in_array('estimate',$this->owner->allowed_actions) ) {
+                $this->js()->univ()->redirect($this->api->url('/'.$this->owner->role.'/quotes/rfq/estimate',
+               			array('quote_id'=>$_GET['estimate'])))->execute();
+            } else {
+                $this->js()->univ()->errorMessage('Action "'.$this->posible_actions['estimate']['name'].'" is not allowed!')->execute();
+            }
+        }
+
+        // requirements
+        if( $_GET['requirements'] ) {
+            if ( in_array('requirements',$this->owner->allowed_actions) ) {
+                $this->js()->univ()->redirect($this->api->url('/'.$this->owner->role.'/quotes/rfq/step2',
+               			array('quote_id'=>$_GET['requirements'])))->execute();
+            } else {
+                $this->js()->univ()->errorMessage('Action "'.$this->posible_actions['requirements']['name'].'" is not allowed')->execute();
+            }
+        }
+
+        // estimation
+        if( $_GET['estimation'] ){
+            if ( in_array('estimation',$this->owner->allowed_actions) ) {
+                $quote=$this->add('Model_Quote')->load($_GET['estimation']);
+               	$quote->set('status','estimate_needed');
+               	$quote->save();
+                $this->js()->reload()->execute();
+            } else {
+                $this->js()->univ()->errorMessage('Action "'.$this->posible_actions['estimation']['name'].'" is not allowed')->execute();
+            }
+        }
+
+        // approve
+        if( $_GET['approve'] ){
+            if ( in_array('approve',$this->owner->allowed_actions) ) {
+                $quote=$this->add('Model_Quote')->load($_GET['approve']);
+               	$quote->set('status','estimation_approved');
+               	$quote->save();
+                $this->js()->reload()->execute();
+            } else {
+                $this->js()->univ()->errorMessage('Action "'.$this->posible_actions['approve']['name'].'" is not allowed')->execute();
+            }
+        }
+
+        // send_to_client
+        if( $_GET['send_to_client'] ){
+            if ( in_array('send_to_client',$this->owner->allowed_actions) ) {
+                $this->sendEmailToClient($_GET['send_to_client']);
+            } else {
+                $this->js()->univ()->errorMessage('Action "'.$this->posible_actions['send_to_client']['name'].'" is not allowed')->execute();
+            }
+        }
+
     }
     function setModel($model, $actual_fields = UNDEFINED) {
+        $this->addColumn('quotation');
+        $this->addColumn('estimate_info');
+
         parent::setModel($model, $actual_fields);
+
+        // remove columns
+        $this->removeColumn('project');
         $this->removeColumn('user');
         $this->removeColumn('name');
         $this->removeColumn('estimpay');
         $this->removeColumn('rate');
         $this->removeColumn('currency');
         $this->removeColumn('spent_time');
-//        $this->addColumn('status');
+        $this->removeColumn('estimated');
+
+        // formatters
         $this->addFormatter('status','wrap');
+
+
+        // add columns after model columns
+        $this->addColumn('actions');
     }
     function formatRow() {
     	parent::formatRow();
-
     	//$this->js('click')->_selector('[data-id='.$this->current_row['id'].']')->univ()->redirect($this->current_row['id']);
 
-    	$this->current_row_html['requirements']=($this->current_row['status']=='Quotation Requested')?$this->current_row_html['requirements']:'';
-    	$this->current_row_html['approve']=($this->current_row['status']=='Estimated')?$this->current_row_html['approve']:'';
-    	$this->current_row_html['estimation']=($this->current_row['status']=='Quotation Requested')?$this->current_row_html['estimation']:'';
-    	$this->current_row_html['send_to_client']=($this->current_row['status']=='Estimated')?$this->current_row_html['send_to_client']:'';
-    	$this->current_row_html['estimate']=($this->current_row['status']=='Estimate Needed')?$this->current_row_html['estimate']:'';
     	if ($this->api->auth->model['is_client']){
 	    	if ( ($this->current_row['status']=='Not Estimated') || ($this->current_row['status']=='Quotation Requested') ){
 	    		//$this->current_row_html['edit']="<button type=\"button\" class=\"button_edit\" onclick=\"$(this).univ().ajaxec('/public/?page=client/quotes&edit=".$this->current_row['id']."&colubris_client_quotes_client_quotes_grid_quotes_edit=".$this->current_row['id']."')\">Edit</button>";
@@ -32,7 +109,7 @@ class Grid_Quotes extends Grid_Advanced {
 	    	}
     	}
 
-        $this->current_row_html['project'] =
+        $this->current_row_html['quotation'] =
                 '<div class="quote_name"><a href="'.$this->api->url('/manager/quotes/rfq/view',array('quote_id'=>$this->current_row['id'])).'">'.$this->current_row['name'].'</a></div>'.
                 '<div class="quote_project"><span>Project:</span>'.$this->current_row['project'].'</div>'.
                 '<div class="quote_client"><span>User:</span>'.$this->current_row['user'].'</div>'
@@ -66,12 +143,22 @@ class Grid_Quotes extends Grid_Advanced {
             $this->current_row['estimpay'] = '-';
         }
 
-        $this->current_row_html['estimated'] =
+        $this->current_row_html['estimate_info'] =
                 '<div class="quote_estimated"><span>Est.time:</span>'.$this->current_row['estimated'].'</div>'.
                 '<div class="quote_rate"><span>Rate:</span>'.$this->current_row['rate'].'</div>'.
                 '<div class="quote_estimpay"><span>Est.pay:</span>'.$this->current_row['estimpay'].'</div>'.
                 '<div class="quote_spent_time"><span>Spent:</span>'.$this->current_row['spent_time'].'</div>'
         ;
+
+        // actions
+        $v = $this->add('View','action_'.$this->current_id,'content');
+        foreach ($this->owner->allowed_actions as $action) {
+            if ($this->current_row['status'] == $this->posible_actions[$action]['status']) {
+                $v->add('View')->set($this->posible_actions[$action]['name'])->addClass('a_look')
+                        ->js('click')->univ()->ajaxec($this->api->url(null,array($this->posible_actions[$action]['get_var']=>$this->current_id)));
+            }
+        }
+        $this->current_row_html['actions'] = $v->getHTML();
     }
     function format_status($field){
     	switch($this->current_row[$field]){
@@ -109,8 +196,29 @@ class Grid_Quotes extends Grid_Advanced {
     function format_durdead($field){
     	echo $this->current_row['duration'];
     }
+
     function defaultTemplate() {
     	return array('grid/colored');
+    }
+
+    function sendEmailToClient($quote_id) {
+        $quote=$this->add('Model_Quote')->load($quote_id);
+       	if ($quote['client_id']>0){
+       		$client=$this->add('Model_Client')->load($quote['client_id']);
+               $this->api->mailer->setReceivers(array($client['email']));
+
+       		if ($client['email']!=''){
+       			$this->api->mailer->sendMail('send_quote',array(
+                       'link'=>$this->api->siteURL().$this->api->url('client/quotes/rfq/estimated',array('quote_id'=>$quote_id))
+                   ),true);
+
+	            $this->js()->univ()->successMessage('Mail sent to '.$client['email'])->execute();
+       		} else {
+       			$this->js()->univ()->successMessage('Error! The client '.$client->get('name').' has no email. Please add email for the client.')->execute();
+       		}
+       	} else {
+       		$this->js()->univ()->successMessage('The project of this quote has no client!')->execute();
+       	}
     }
     
     function precacheTemplate() {
