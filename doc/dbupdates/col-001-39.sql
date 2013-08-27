e241ee69f64a1166e60fcf3f07ec543a51ec9052
vadym <vadym@xxx.(none)>
2013-08-27 11:49:58 +0300

    migration fix

--- doc/dbupdates/col-001-39.sql
+++ doc/dbupdates/col-001-39.sql
@@ -1,2 +1,2 @@
 /*[6:59:35 PM][631 ms]*/
-ALTER TABLE `colubris`.`requirement` ADD COLUMN `is_included` TINYINT(1) DEFAULT 1 NOT NULL AFTER `file_id`;
+ALTER TABLE `requirement` ADD COLUMN `is_included` TINYINT(1) DEFAULT 1 NOT NULL AFTER `file_id`;
