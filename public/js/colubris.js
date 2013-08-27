05366b1faa4b280f077f356c9e4ce999ac254f63
vadym <vadym@xxx.(none)>
2013-08-27 11:45:51 +0300

    not all requirements going to quotation

---
+++ public/js/colubris.js
@@ -0,0 +1,77 @@
+(function($){
+$.colubris=function(){
+    return $.colubris;
+}
+	
+$.fn.extend({colubris:function(){
+	var u=new $.colubris;
+	u.jquery=this;
+	return u;
+}});
+	
+$.colubris._import=function(name,fn){
+	$.colubris[name]=function(){
+		var ret=fn.apply($.colubris,arguments);
+		return ret?ret:$.colubris;
+	}
+}
+	
+$.each({
+	alert: function(text){
+		alert(text);		
+	},
+    toggle_is_included: function(url,reload_views){
+        $(".toggle_is_included").each(function(i,e){
+            $(e).bind("click",function(ev){
+                ev.preventDefault();ev.stopPropagation();
+                $(this).univ().ajaxec(url,{'req_id':$(this).attr('data-id')});
+            });
+        });
+	},
+    floating_total: function(el_id){
+
+        var min_top = 20;
+        var max_top = 390;
+        var top = max_top;
+        var window_top;
+        var window_height;
+
+        var el = $('#' + el_id);
+
+        setCSS(el);
+
+        $(window).scroll(function (event) {
+            setCSS(el);
+        });
+        $(window).resize(function (event) {
+            setCSS(el);
+        });
+
+        function setCSS(element) {
+            window_top = $(window).scrollTop();
+            window_height = $(window).height();
+
+            if ( window_top < max_top ) {
+                top = max_top - window_top;
+            } else if ( window_top > min_top ) {
+                top = min_top;
+            }
+
+            // if window is too small
+            if ((window_height - 50) < top) {
+                top = window_height - 50;
+            }
+
+            element
+                    .css('position','fixed')
+                    .css('top',top + 'px')
+                    .css('right','50px')
+                    .css('z-index','5')
+            ;
+        }
+
+	}
+	
+},$.colubris._import);
+	
+})(jQuery);
