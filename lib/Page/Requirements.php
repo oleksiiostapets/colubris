522f0526fcbf4c441908d234f62a61851b9b2532
alexeyostapets <oleksii.ostapets@gmail.com>
2013-09-09 18:16:31 +0300

    fixes

--- lib/Page/Requirements.php
+++ lib/Page/Requirements.php
@@ -282,7 +282,12 @@ class Page_Requirements extends Page {
         );
 
         $requirements->addExpression('cost')->set(function($m,$q)use($quote){
-            return "concat(round(estimate * ".$quote['rate']."),' ".$quote['currency']."' )";
+            if($quote['rate']!=''){
+                return "concat(round(estimate * ".$quote['rate']."),' ".$quote['currency']."' )";
+            }
+            else{
+                return true;
+            }
         });
         if ($this->api->currentUser()->isCurrentUserClient()){
             $requirements->getElement('estimate')->destroy();
