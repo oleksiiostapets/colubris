d84f1ba5828cf32b8c8a696d809bac7850e9cb17
alexeyostapets <oleksii.ostapets@gmail.com>
2013-09-10 15:21:27 +0300

    fixes

--- lib/Model/Reqcomment.php
+++ lib/Model/Reqcomment.php
@@ -1,38 +1,8 @@
 <?
-class Model_Reqcomment extends Model_Auditable {
-	public $table='reqcomment';
-	function init(){
-		parent::init();
-		$this->hasOne('Requirement');
-		$this->hasOne('User')->Caption('Creator');
-		$this->addField('text')->type('text')->mandatory('required');
+class Model_Reqcomment extends Model_Reqcomment_Base {
+    function init(){
+        parent::init(); //$this->debug();
 
-        $attach = $this->add('filestore/Field_Image','file_id')->setModel('Model_Myfile');
-              $attach->addThumb();
-		
-		$this->addField('created_dts')->Caption('Created At')->sortable(true);
-		
-		$this->addHook('beforeInsert',function($m,$q){
-			$q->set('user_id',$q->api->auth->model['id']);
-        	$q->set('created_dts', $q->expr('now()'));
-		});
-		
-        $this->addHook('beforeSave',function($m){
-        	if($m['user_id']>0){
-	        	if($m->api->auth->model['id']!=$m['user_id']){
-	        		throw $m
-		                ->exception('You have no permissions to do this','ValidityCheck')
-		                ->setField('text');
-	        	}
-        	}
-        });
-        $this->addHook('beforeDelete',function($m){
-        	if($m['user_id']>0){
-	        	if($m->api->auth->model['id']!=$m['user_id']){
-	        		throw $m
-		                ->exception('You have no permissions to do this','ValidityCheck');
-	        	}
-        	}
-        });
-	}
+        $this->addCondition('is_deleted',false);
+    }
 }
