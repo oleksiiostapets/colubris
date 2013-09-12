f21f8ce78503ae463f7f5ddc8192c1e0b0d122eb
vadym <vadym@xxx.(none)>
2013-09-12 13:31:07 +0300

    migrations

--- doc/dbupdates/col-001-48.sql
+++ doc/dbupdates/col-001-48.sql
@@ -1,2 +1,4 @@
 /*[12:50:54 PM][514 ms]*/
-ALTER TABLE `colubris`.`requirement` ADD COLUMN `is_deleted` TINYINT(1) DEFAULT 0 NOT NULL AFTER `is_included`;
+ALTER TABLE `requirement` ADD COLUMN `is_deleted` TINYINT(1) DEFAULT 0 NOT NULL AFTER `is_included`;
+ALTER TABLE `attach` ADD COLUMN `is_deleted` TINYINT(1) DEFAULT 0 NOT NULL AFTER `task_id`;
+ALTER TABLE `taskcomment` ADD COLUMN `is_deleted` TINYINT(1) DEFAULT 0 NOT NULL AFTER `created_dts`;
