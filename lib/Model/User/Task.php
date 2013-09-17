22f25b5e071f94ef5b731318a4d5f4be0546a297
alexeyostapets <oleksii.ostapets@gmail.com>
2013-09-17 13:52:29 +0300

    fix

--- lib/Model/User/Task.php
+++ lib/Model/User/Task.php
@@ -3,9 +3,8 @@ class Model_User_Task extends Model_User_Organisation {
     function init(){
         parent::init();
 
-        $this->api->stickyGet('project_id');
         $p=$this->add('Model_Project');
-        $par_ids=$p->getAllParticipants($_GET['project_id']);
+        $par_ids=$p->getAllParticipants($this->api->recall('project_id'));
         $this->addCondition('id','in',implode(',',$par_ids));
     }
 }
