d74334d28147aaf98da1b79c20ea0d45fdbccd6b
alexeyostapets <oleksii.ostapets@gmail.com>
2013-09-20 12:53:02 +0300

    task comments on dashboard

---
+++ lib/Model/Taskcomment/Developer.php
@@ -0,0 +1,17 @@
+<?
+class Model_Taskcomment_Developer extends Model_Taskcomment {
+	function init(){
+		parent::init();
+        //$this->debug();
+        $participated_in=$this->add('Model_Project')->forDeveloper();
+        $projects_ids=array(0);
+        foreach($participated_in as $p){
+            $projects_ids[]=$p['id'];
+        }
+        $jt = $this->join('task.id','task_id','left','_t');
+
+        $jt->addField('project_id','project_id');
+
+        $this->addCondition('project_id','IN',$projects_ids);
+	}
+}
