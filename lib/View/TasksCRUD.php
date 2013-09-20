56f254c38eb9ba1f7e4b92f7d0b2051f51d2c82d
alexeyostapets <oleksii.ostapets@gmail.com>
2013-09-20 14:21:31 +0300

    fix

--- lib/View/TasksCRUD.php
+++ lib/View/TasksCRUD.php
@@ -26,19 +26,16 @@ class View_TasksCRUD extends View {
         if($cr->grid){
             $cr->grid->addClass('zebra bordered');
             $cr->grid->addPaginator(10);
-        	//$cr->grid->js('reload')->reload();
-        	
-        	if(!$this->api->auth->model['is_client']){
-   	        	$cr->grid->addColumn('expander','time');
-                /*
-	            if ($_GET['time']) {
-	                $this->js()->univ()->frameURL($this->api->_('Time'),array(
-	                    $this->api->url('./time',array('task_id'=>$_GET['time'],'reload_view'=>$cr->grid->name))
-	                ))->execute();
-                    $this->js()->redirect()->execute();
-	            }
-                */
-        	}
+        	$cr->grid->js('reload')->reload();
+
+            if(!$this->api->auth->model['is_client']){
+                $cr->grid->addColumn('button','time');
+                if ($_GET['time']) {
+                    $this->js()->univ()->frameURL($this->api->_('Time'),array(
+                        $this->api->url('./time',array('task_id'=>$_GET['time'],'reload_view'=>$cr->grid->name))
+                    ))->execute();
+                }
+            }
             $cr->grid->addColumn('expander','more');
         	$cr->grid->addFormatter('status','status');
             $cr->grid->addFormatter('name','wrap');
