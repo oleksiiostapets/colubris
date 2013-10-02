c77f2c712600a7428ede97355906fcaff2f3952a
KonstantinKolodnitsky <stakantin@mail.ru>
2013-10-02 16:48:37 +0300

    implementing kk_xls addon

--- lib/View/Report.php
+++ lib/View/Report.php
@@ -60,94 +60,21 @@ class View_Report extends View {
 
         $v=$this->add('View');
         $v->setClass('right');
-        $export_button=$v->add('Button')->set('Export to XLS');
-        $export_button->js('click',
-            $this->js()->univ()->redirect($this->api->url(null,array('action'=>'export')))
+        $properties = array(
+            'creator'        => 'Oleksii Ostapets',
+            'lastModifiedBy' => 'Oleksii Ostapets',
+            'title'          => 'Colubris report',
+            'subject'        => 'Colubris report',
+            'description'    => 'Colubris report',
+            'keywords'       => 'Colubris report',
+            'category'       => 'Colubris report'
         );
-
-        if ($_GET['action']=='export'){
-            /*
-            header('Content-Disposition: attachment; filename="report.csv"');
-            $header=implode(";",$this->export_fields).";";
-
-            $total_spent=0;
-            $data="";
-            foreach($m->getRows() as $row){
-                $total_spent+=$row['spent_time'];
-                foreach($this->export_fields as $field_name){
-                    $data.=substr($row[$field_name],0,80).";";
-                }
-                $data.="\n";
-            }
-            $data.="TOTAL; ; ; ; ;$total_spent; ; ;";
-
-            print "$header\n$data";
-            */
-            require_once '../lib/PHPExcel.php';
-            $objPHPExcel = new PHPExcel();
-            $objPHPExcel->getProperties()->setCreator("Oleksii Ostapets")
-                ->setLastModifiedBy("Oleksii Ostapets")
-                ->setTitle("Colubris report")
-                ->setSubject("Colubris report")
-                ->setDescription("Colubris report")
-                ->setKeywords("colubris report")
-                ->setCategory("Colubris report");
-
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(0))->setWidth(15);
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(1))->setWidth(30);
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(2))->setWidth(10);
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(3))->setWidth(14);
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(4))->setWidth(12);
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(5))->setWidth(12);
-            $objPHPExcel->getActiveSheet()->getColumnDimension($this->getColumnIndex(6))->setWidth(15);
-
-            for($i=0; $i<count($this->export_fields); $i++){
-                $objRichText = new PHPExcel_RichText();
-                $objPayable = $objRichText->createTextRun($this->export_fields[$i]);
-                $objPayable->getFont()->setBold(true);
-                $objPHPExcel->getActiveSheet()->setCellValue($this->getColumnIndex($i).'1', $objRichText);
-            }
-            $objPHPExcel->getActiveSheet()->getRowDimension(1)->setRowHeight(20);
-
-            $data=$m->getRows();
-            $total_spent=0;
-            for($i=0; $i<count($data); $i++){
-                $objPHPExcel->getActiveSheet()->getRowDimension($i+2)->setRowHeight(20);
-                for($j=0; $j<count($this->export_fields); $j++){
-                    $objPHPExcel->setActiveSheetIndex(0)->setCellValue($this->getColumnIndex($j).($i+2),$data[$i][$this->export_fields[$j]]);
-                }
-                $total_spent=$total_spent+(float)$data[$i]['spent_time'];
-            }
-
-            $objRichText = new PHPExcel_RichText();
-            $objPayable = $objRichText->createTextRun('TOTAL');
-            $objPayable->getFont()->setBold(true);
-            $objPHPExcel->getActiveSheet()->setCellValue($this->getColumnIndex(0).($i+2), $objRichText);
-
-            $objRichText = new PHPExcel_RichText();
-            $objPayable = $objRichText->createTextRun($total_spent);
-            $objPayable->getFont()->setBold(true);
-            $objPHPExcel->getActiveSheet()->setCellValue($this->getColumnIndex(5).($i+2), $objRichText);
-
-            $objPHPExcel->getActiveSheet()->getRowDimension($i+2)->setRowHeight(20);
-
-            header('Content-Type: application/vnd.ms-excel');
-            header('Content-Disposition: attachment;filename="report-colubris-'.date('Y-m-i-H-i-s').'.xls"');
-            header('Cache-Control: max-age=0');
-// If you're serving to IE 9, then the following may be needed
-            header('Cache-Control: max-age=1');
-
-// If you're serving to IE over SSL, then the following may be needed
-            header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-            header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-            header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-            header ('Pragma: public'); // HTTP/1.0
-
-            $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-            $objWriter->save('php://output');
-
-            exit;
-        }
+        $fields = array('project_name', 'task_name', 'status', 'type', 'estimate', 'spent_time','date');
+        $v->add('kk_xls\View_ButtonXLS',array(
+            'data'       => $m,
+            'properties' => $properties,
+            'fields'     => $fields
+        ))->set('Export to XLS');
 
         $v=$this->add('View');
         $v->setClass('cc');
