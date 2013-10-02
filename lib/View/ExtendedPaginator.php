88dec7d5c1c86c780b9a241d7578f39217fe1bee
alexeyostapets <oleksii.ostapets@gmail.com>
2013-10-02 10:03:08 +0300

    try

--- lib/View/ExtendedPaginator.php
+++ lib/View/ExtendedPaginator.php
@@ -9,14 +9,16 @@ class View_ExtendedPaginator extends View {
         }elseif ( (isset($_COOKIE['paginatorcount'])) && ($_COOKIE['paginatorcount']>0) ){
             $default_count=$_COOKIE['paginatorcount'];
         }else{
-//            setcookie('paginatorcount',$default_count,time()+60*60*24*30*6);
             $default_count=10;
+            setcookie('paginatorcount',$default_count,time()+60*60*24*30*6);
+/*
             setcookie(
                 'paginatorcount',
                 $default_count,
                 time()+60*60*24*7*30*12,
                 $this->api->url('/')->useAbsoluteUrl()
             );
+*/
         }
 
         $v=$this->add('View')->setClass('paginator_count');
