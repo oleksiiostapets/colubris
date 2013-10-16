41f38268f395465d020eb2849daf1a31a413ace9
KonstantinKolodnitsky <stakantin@mail.ru>
2013-10-16 15:10:10 +0300

    del peoject_id from url

--- lib/Grid/Quotes.php
+++ lib/Grid/Quotes.php
@@ -148,8 +148,8 @@ class Grid_Quotes extends Grid {
 
         $this->current_row_html['quotation'] =
                 '<div class="quote_name"><a href="'.$this->api->url('quotes/rfq/requirements',array(
-                    'quote_id'=>$this->current_row['id'],
-                    'project_id' => $this->current_row['project_id']
+                    'quote_id'=>$this->current_row['id']
+//                    ,'project_id' => $this->current_row['project_id']
                 )).'">'.$this->current_row['name'].'</a></div>'.
                 '<div class="quote_project"><span>Project:</span>'.$this->current_row['project'].'</div>'.
                 '<div class="quote_client"><span>User:</span>'.$this->current_row['user'].'</div>'
