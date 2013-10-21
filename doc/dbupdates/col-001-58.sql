b0dbdaaa50dd1e8cca16b6816841adacc6d7b03b
alexeyostapets <oleksii.ostapets@gmail.com>
2013-10-21 15:13:32 +0300

    fix for deleting quotes

---
+++ doc/dbupdates/col-001-58.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `quote` DROP FOREIGN KEY `fk_quote_project1` ;
+ALTER TABLE `quote`
+  ADD CONSTRAINT `fk_quote_project1`
+  FOREIGN KEY (`project_id` )
+  REFERENCES `project` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
