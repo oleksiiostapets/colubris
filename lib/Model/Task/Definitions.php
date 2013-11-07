833530ec9c61b8117aa9e02f12df474385231350
alexeyostapets <oleksii.ostapets@gmail.com>
2013-11-07 20:06:18 +0200

    up

--- lib/Model/Task/Definitions.php
+++ lib/Model/Task/Definitions.php
@@ -80,7 +80,7 @@ class Model_Task_Definitions extends Model_Auditable {
             $m->api->mailer->addReceiverByUserId($m->get('requester_id'),'mail_task_changes');
             $m->api->mailer->addReceiverByUserId($m->get('assigned_id'),'mail_task_changes');
             $m->api->mailer->sendMail('task_edit',array(
-                'link'=>$m->api->siteURL().$m->api->url('/task',array('task_id'=>$m->get('id'))),
+                'link'=>$m->api->siteURL().$m->api->url('/task',array('task_id'=>$m->get('id'),'colubris_task_view_view_crud_virtualpage'=>null)),
                 'subject'=>'Task "'.substr($m->get('name'),0,25).'" has changes',
                 'changer_part'=>$m->api->currentUser()->get('name').' has made changes in task "'.$m->get('name').'".',
             ));
--- lib/Model/Taskcomment/Base.php
+++ lib/Model/Taskcomment/Base.php
@@ -37,7 +37,7 @@ class Model_Taskcomment_Base extends Model_Auditable {
             $m->api->mailer->addReceiverByUserId($task->get('requester_id'),'mail_task_changes');
             $m->api->mailer->addReceiverByUserId($task->get('assigned_id'),'mail_task_changes');
             $m->api->mailer->sendMail('task_comment_changed',array(
-                'link'=>$m->api->siteURL().$m->api->url('/task',array('task_id'=>$m->get('task_id'),'colubris_task_view_view_2_crud_virtualpage_id'=>null,'colubris_task_view_view_2_crud_virtualpage'=>null)),
+                'link'=>$m->api->siteURL().$m->api->url('/task',array('task_id'=>$m->get('task_id'),'colubris_task_view_view_2_crud_virtualpage_id'=>null,'colubris_task_view_view_2_crud_virtualpage'=>null,'colubris_task_view_view_crud_virtualpage'=>null)),
                 'subject'=>'Task "'.substr($task->get('name'),0,25).'" has changes in comments',
                 'changer_part'=>$m->api->currentUser()->get('name').' has made changes in task "'.$task->get('name').'".',
             ));
