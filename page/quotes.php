3c9dfe4220388085c5d7682c88a9063126d287d0
alexeyostapets <oleksii.ostapets@gmail.com>
2013-11-07 17:42:32 +0200

    quotes tabs

--- page/quotes.php
+++ page/quotes.php
@@ -1,6 +1,6 @@
 <?php
 class page_quotes extends Page {
-    public $quote;
+    public $active_tab=0;
     function init() {
         parent::init();
 
@@ -19,13 +19,17 @@ class page_quotes extends Page {
 
         $tabs = $this->add('Tabs');
 
-        $tabs->addTabUrl('./requested','Requested');
-        $tabs->addTabUrl('./estimate_needed','Estimate Needed');
-        $tabs->addTabUrl('./not_estimated','Not Estimated');
-        $tabs->addTabUrl('./estimated','Estimated');
-        $tabs->addTabUrl('./estimation_approved','Estimation Approved');
-        $tabs->addTabUrl('./finished','Finished');
-        $tabs->addTabUrl('./archived','Archived');
+        $tabs->addTabUrl('./requested','Requested ('.$this->getModelRequested()->count().')');
+        $tabs->addTabUrl('./estimate_needed','Estimate Needed ('.$this->getModelEstimateNeeded()->count().')');
+        $tabs->addTabUrl('./not_estimated','Not Estimated ('.$this->getModelNotEstimated()->count().')');
+        $tabs->addTabUrl('./estimated','Estimated ('.$this->getModelEstimated()->count().')');
+        $tabs->addTabUrl('./estimation_approved','Estimation Approved ('.$this->getModelEstimationApproved()->count().')');
+        $tabs->addTabUrl('./finished','Finished ('.$this->getModelFinished()->count().')');
+        $tabs->addTabUrl('./archived','Archived ('.$this->getModelArchived()->count().')');
+
+        if ($_GET['active_tab']>0){
+            $this->api->memorize('active_tab',$_GET['active_tab']);
+        }
 
     }
     function getBaseModel(){
@@ -46,52 +50,80 @@ class page_quotes extends Page {
 
         return $quote;
     }
-    function page_requested() {
+    function getModelRequested(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',false);
         $quote->addCondition('status','quotation_requested');
-
+        return $quote;
+    }
+    function page_requested() {
+        $this->active_tab=1;
+        $quote=$this->getModelRequested();
         $this->addQuotesCRUD($this,$quote,'active');
     }
-    function page_estimate_needed() {
+    function getModelEstimateNeeded(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',false);
         $quote->addCondition('status','estimate_needed');
-
+        return $quote;
+    }
+    function page_estimate_needed() {
+        $this->active_tab=2;
+        $quote=$this->getModelEstimateNeeded();
         $this->addQuotesCRUD($this,$quote,'active');
     }
-    function page_not_estimated() {
+    function getModelNotEstimated(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',false);
         $quote->addCondition('status','not_estimated');
-
+        return $quote;
+    }
+    function page_not_estimated() {
+        $this->active_tab=3;
+        $quote=$this->getModelNotEstimated();
         $this->addQuotesCRUD($this,$quote,'active');
     }
-    function page_estimated() {
+    function getModelEstimated(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',false);
         $quote->addCondition('status','estimated');
-
+        return $quote;
+    }
+    function page_estimated() {
+        $this->active_tab=4;
+        $quote=$this->getModelEstimated();
         $this->addQuotesCRUD($this,$quote,'active');
     }
-    function page_estimation_approved() {
+    function getModelEstimationApproved(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',false);
         $quote->addCondition('status','estimation_approved');
-
+        return $quote;
+    }
+    function page_estimation_approved() {
+        $this->active_tab=5;
+        $quote=$this->getModelEstimationApproved();
         $this->addQuotesCRUD($this,$quote,'active');
     }
-    function page_finished() {
+    function getModelFinished(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',false);
         $quote->addCondition('status','finished');
-
+        return $quote;
+    }
+    function page_finished() {
+        $this->active_tab=6;
+        $quote=$this->getModelFinished();
         $this->addQuotesCRUD($this,$quote,'active');
     }
-    function page_archived() {
+    function getModelArchived(){
         $quote=$this->getBaseModel();
         $quote->addCondition('is_archived',true);
-
+        return $quote;
+    }
+    function page_archived() {
+        $this->active_tab=7;
+        $quote=$this->getModelArchived();
         $this->addQuotesCRUD($this,$quote,'archive');
     }
 
@@ -143,14 +175,16 @@ class page_quotes extends Page {
                     $mq->in_archive();
                     $mq->save();
 
-                    $cr->grid->js()->reload()->execute();
+                    $this->js()->univ()->redirect('quotes',array('active_tab'=>$this->active_tab))->execute();
+                    //$cr->grid->js()->reload()->execute();
                 }
                 if($_GET['activate']){
                     $mq=$this->add('Model_Quote')->load($_GET['activate']);
                     $mq->activate();
                     $mq->save();
 
-                    $cr->grid->js()->reload()->execute();
+                    $this->js()->univ()->redirect('quotes',array('active_tab'=>$this->active_tab))->execute();
+                    //$cr->grid->js()->reload()->execute();
                 }
             }
 
@@ -164,5 +198,10 @@ class page_quotes extends Page {
             $cr->grid->addQuickSearch(array('quote.name','project'));
         }
 
+    }
+    function defaultTemplate() {
+        $tab=$this->api->recall('active_tab');
+        $this->api->forget('active_tab');
+        return array('page/quotes/base'.$tab);
     }
 }
