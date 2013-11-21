632d296f5bd2ffa1ea99f3ba6d9afb297cf6aef7
alexeyostapets <oleksii.ostapets@gmail.com>
2013-11-21 09:55:29 +0200

    extended requirement page

--- lib/Grid/Quote.php
+++ lib/Grid/Quote.php
@@ -11,4 +11,15 @@ class Grid_Quote extends Grid {
 
         $this->current_row_html['general_description']=nl2br($this->current_row_html['general_description']);
     }
+    function format_download($field){
+        if(strpos($this->current_row[$field],'upload/')===false){
+            $this->current_row_html[$field]=$this->current_row[$field];
+        }else{
+            if ($this->current_row[$field]!=''){
+                $this->current_row_html[$field]='<a target="_blank" href="'.$this->current_row[$field].'"/>file</a>';
+            }else{
+                $this->current_row_html[$field]='';
+            }
+        }
+    }
 }
