3679630c438c9915de6a3315a1fee52cf301efbc
vadym <vadym@xxx.(none)>
2014-01-07 14:03:03 +0200

    user can see spent time in time based quotes (fix)

--- lib/View/Requirement.php
+++ lib/View/Requirement.php
@@ -119,10 +119,14 @@ class View_Requirement extends AbstractView {
             'allow_del'       => $can_del,
         ));
 
+        if ($cr->grid) {
+            $cr->grid->quote = $this->quote;
+        }
+
         $cr->setModel(
             $this->tasks,
             $this->tasks->whatFieldsUserCanEdit($user),
-            $this->tasks->whatFieldsUserCanSee($user)
+            $this->tasks->whatFieldsUserCanSee($user,$this->quote)
         );
     }
 
