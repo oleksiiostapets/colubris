c8f826ab59796152984ba5dd1ebc73d098e519d7
alexeyostapets <alf.moc@gmail.com>
2014-04-11 17:24:59 +0300

    fix filter

--- shared/Model/User/Task.php
+++ shared/Model/User/Task.php
@@ -8,7 +8,7 @@ class Model_User_Task extends Model_User_Organisation {
             $par_ids=$p->getAllParticipants($this->api->recall('project_id'));
             $this->addCondition('id','in',implode(',',$par_ids));
         }*/
-        if ($_GET['project_id']){
+        if ($_GET['project']){
             $p=$this->add('Model_Project');
             $par_ids=$p->getAllParticipants($_GET['project_id']);
             $this->addCondition('id','in',implode(',',$par_ids));
