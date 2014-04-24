a623564cae2da67cb05f351ffd7afcd4e72d84b4
KonstantinKolodnitsky <stakantin@mail.ru>
2014-04-24 17:09:41 +0300

    fix deleted page

--- page/deleted.php
+++ page/deleted.php
@@ -5,7 +5,7 @@ class page_deleted extends Page {
         parent::init();
 
         // Checking client's read permission to this quote and redirect to denied if required
-        if( !$this->api->currentUser()->canSeeDeleted() ){
+        if( !$this->app->user_access->canSeeDeleted() ){
             throw $this->exception('You cannot see this page','Exception_Denied');
         }
     }
