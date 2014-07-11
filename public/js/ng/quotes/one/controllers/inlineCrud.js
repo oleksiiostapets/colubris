e1bbf9fbadaf6538d8487c4766478d6b902392cd
alexeyostapets <alf.moc@gmail.com>
2014-07-11 12:42:58 +0300

    ESC closes requirements popup

--- public/js/ng/quotes/one/controllers/inlineCrud.js
+++ public/js/ng/quotes/one/controllers/inlineCrud.js
@@ -21,5 +21,11 @@ app_module.controller('inlineCrud', ['$scope','$http','Requirement', function ($
     $scope.$on( 'requirements.update', function( event ) {
         $scope.requirements = Requirement.requirements;
     });
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        if (evt.keyCode == 27) {
+            if(!$.isEmptyObject(Requirement.requirements[0])) Requirement.cancel();
+        }
+    };
 }])
 ;
