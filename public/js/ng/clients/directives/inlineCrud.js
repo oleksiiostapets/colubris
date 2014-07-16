7f6b47a3d4995e16adf7a2f893161c866708343c
KonstantinKolodnitsky <stakantin@mail.ru>
2014-07-16 09:38:32 +0300

    add page clients with angularjs, fix css for floaring forms

---
+++ public/js/ng/clients/directives/inlineCrud.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vadym on 6/13/14.
+ */
+
+'use strict';
+
+app_module.directive('inlineCrud', function factory($q,$http, $templateCache,$compile) {
+    return function(scope,element,attrs) {
+
+        scope.form = app_module.base_url + 'js/ng/clients/templates/form.html';
+        scope.crud = app_module.base_url + 'js/ng/clients/templates/crud.html';
+//        scope.comment_list = app_module.base_url + 'js/ng/quotes/one/templates/comment-list.html';
+//        scope.task_list = app_module.base_url + 'js/ng/quotes/one/templates/task-list.html';
+    }
+})
+;
