546ce758b62e008c1a95b4b25686160cdc5e98d1
Oleksii Ostapets <alf.moc@gmail.com>
2014-08-19 15:38:54 +0300

    api

--- page/testapi.php
+++ page/testapi.php
@@ -16,7 +16,9 @@ class page_testapi extends Page {
 
         if ($f->isSubmitted()){
             //var_dump($f->get());
-            $url = 'http://localhost/colubris43/api/?page=v1/user/login/';
+            $absolute_url = $this->full_url($_SERVER);
+            //$url = 'http://localhost/colubris43/api/?page=v1/user/login/';
+            $url = $absolute_url.'/api/?page=v1/user/login/';
             $data = array('u'=>$f->get('u'),'p'=>$f->get('p'));
             $res = $this->do_post_request($url,$data);
             if($res == 'false') {
@@ -27,7 +29,21 @@ class page_testapi extends Page {
             }
         }
     }
-
+    function url_origin($s, $use_forwarded_host=false)
+    {
+        $ssl = (!empty($s['HTTPS']) && $s['HTTPS'] == 'on') ? true:false;
+        $sp = strtolower($s['SERVER_PROTOCOL']);
+        $protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
+        $port = $s['SERVER_PORT'];
+        $port = ((!$ssl && $port=='80') || ($ssl && $port=='443')) ? '' : ':'.$port;
+        $host = ($use_forwarded_host && isset($s['HTTP_X_FORWARDED_HOST'])) ? $s['HTTP_X_FORWARDED_HOST'] : (isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : null);
+        $host = isset($host) ? $host : $s['SERVER_NAME'] . $port;
+        return $protocol . '://' . $host;
+    }
+    function full_url($s, $use_forwarded_host=false)
+    {
+        return $this->url_origin($s, $use_forwarded_host) . substr($s['REQUEST_URI'],0,strpos($s['REQUEST_URI'],'/public'));
+    }
     function page_check(){
         $url = 'http://localhost/colubris43/api/?page=v1/user/check&lhash='.$_GET['lhash'];
         $data = array('lhash'=>$_GET['lhash']);
