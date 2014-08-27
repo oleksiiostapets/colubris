8f89d062cd7a603119fb958a598fb6b2d80e21db
Konstantin <kolodnitsky@gmail.com>
2014-08-27 13:17:17 +0300

    fix user bug

--- shared/lib/Model/User.php
+++ shared/lib/Model/User.php
@@ -77,6 +77,12 @@ class Model_User extends Model_BaseTable {
 				->exception('Do not change email for existing user','ValidityCheck')
 				->setField('email');
 		});
+		$this->addHook('afterLoad',function($m){
+			$m_rights = $this->add('Model_User_Right')->tryLoadBy('user_id',$m->id);
+            if(!$m_rights->loaded()){
+                $m_rights->saveNewUserAsEmpty($m->id);
+            }
+		});
 	}
 
 	// HOOKS :: END -----------------------------------------------------------
