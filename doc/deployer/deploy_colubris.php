67a5a0eb6f193446740852702cc7b4d2c74d6d43
Vadym <vadym@xxx-2.local>
2014-09-10 17:35:46 +0700

    deployer update (no local instalation)

--- doc/deployer/deploy_colubris.php
+++ doc/deployer/deploy_colubris.php
@@ -6,75 +6,6 @@
 include './deployer.phar';
 include './config.php';
 
-// ---------------------------------------------------------------------------------------------------
-//
-//                local
-//
-// ---------------------------------------------------------------------------------------------------
-
-$t = new Transport_Local();
-
-$server_deploy_folder = time();
-$base_path = getcwd();
-
-$migr = new Service_AgileToolkit_Migrator('mysql -uvadym -p1 -hlocalhost colubris');
-$migr->setMigrationPath($base_path.'/releases/'.$server_deploy_folder.'/doc/dbupdates');
-$migr->setMigrationStatusesPath($base_path.'/shared/db');
-
-$s1 = new Server('My local computer Colubris project installation');
-$s1->setTransport($t);
-$s1->setBasePath($base_path);
-
-// shared
-$s1->task('cmd','mkdir -m 755 ./shared');
-$s1->task('cmd','mkdir -m 755 ./shared/db');
-$s1->task('cmd','mkdir -m 777 ./shared/upload');
-
-// releases
-$s1->task('cmd','mkdir -m 755 ./releases');
-$s1->task('cmd',"mkdir releases/$server_deploy_folder");
-$s1->task('cmd',"git clone git@github.com:alexeyostapets/colubris.git ./releases/$server_deploy_folder");
-$s1->task('cmd',"cd releases/$server_deploy_folder");
-$s1->task('cmd',"git fetch");
-$s1->task('cmd',"git checkout -b 4.3 origin/4.3");
-$s1->task('cmd',"git pull");
-
-// addons
-$s1->task('cmd',"mkdir addons");
-$s1->task('cmd',"cd addons");
-$s1->task('cmd',"git clone git@github.com:atk4/autocomplete.git");
-$s1->task('cmd',"git clone git@github.com:KonstantinKolodnitsky/kk_xls.git");
-$s1->task('cmd',"git clone git@github.com:rvadym/x_bread_crumb.git");
-$s1->task('cmd',"git clone git@github.com:rvadym/x_breakpoint.git");
-
-// logs
-$s1->task('cmd',"cd $base_path/shared/");
-$s1->task('cmd',"mkdir -m 777 logs");
-
-// composer
-$s1->task('cmd',"rm composer.phar");
-$s1->task('cmd',"curl -sS https://getcomposer.org/installer | php");
-$s1->task('cmd',"cd $base_path/releases/$server_deploy_folder");
-$s1->task('cmd',"ln -s ../../shared/composer.phar composer.phar");
-$s1->task('cmd',"php composer.phar install");
-
-// atk4-addons
-$s1->task('cmd',"cd $base_path/releases/$server_deploy_folder");
-$s1->task('cmd',"git clone git@github.com:atk4/atk4-addons.git");
-
-// links
-$s1->task('cmd',"cd $base_path/releases/$server_deploy_folder");
-$s1->task('cmd',"ln -s ../../shared/logs logs");
-$s1->task('cmd',"ln -s ../../shared/upload upload");
-$s1->task('cmd',"ln -s ../../../shared/upload public/upload");
-$s1->task('cmd',"ln -s ../../shared/config.php config.php");
-$s1->task('cmd',"ln -s ../../../shared/config.php api/config.php");
-
-// final
-$s1->task('cmd',"cd $base_path");
-$s1->task('cmd',"rm current");
-$s1->task('cmd',"ln -s ./releases/$server_deploy_folder/ current");
-
 
 // ---------------------------------------------------------------------------------------------------
 //
@@ -167,6 +98,8 @@ $s2->task('cmd',"cd $base_path");
 $s2->task('cmd',"rm current");
 $s2->task('cmd',"ln -s ./releases/$server_deploy_folder/ current");
 
+// celestial -----------------------------------------------------------------------------------------
+
 
 
 
@@ -177,9 +110,7 @@ $s2->task('cmd',"ln -s ./releases/$server_deploy_folder/ current");
 // ---------------------------------------------------------------------------------------------------
 
 $d = new Deployer();
-//$d->addServer($s1); // local
-$d->addServer($s2);   // celestial
+$d->addServer($s2);
 $d->run();
 
-// deployer --------------------------------------------------------------------------------------------
-
+// deployer ------------------------------------------------------------------------------------------
