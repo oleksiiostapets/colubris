124efbbc211b635fa93822f04e68338c70fb53c5
Konstantin <kolodnitsky@gmail.com>
2014-11-27 18:31:15 +0200

    edit task

--- public/js/ng/tasks/directives/inlineCrud.js
+++ public/js/ng/tasks/directives/inlineCrud.js
@@ -7,10 +7,9 @@
 app_module.directive('inlineCrud', function factory($q,$http, $templateCache,$compile) {
     return function(scope,element,attrs) {
 
-//        scope.form = app_module.base_url + 'js/ng/tasks/templates/form.html';
+        scope.form = app_module.base_url + 'js/ng/tasks/templates/form.html';
         scope.crud = app_module.base_url + 'js/ng/tasks/templates/crud.html';
 //        scope.comment_list = app_module.base_url + 'js/ng/quotes/one/templates/comment-list.html';
-//        scope.task_list = app_module.base_url + 'js/ng/quotes/one/templates/task-list.html';
     }
 })
 ;
