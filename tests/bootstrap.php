d2dc9934941eba787ef9d0ac243272d317069777
Vadym <vadym@agile55.com>
2014-12-17 18:38:09 +0200

    UI tests preparations

---
+++ tests/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadym
+ * Date: 17/12/14
+ * Time: 23:32
+ */
+
+
+
+
+// ---------------------------------------------------------------------------------------------------
+//
+//  Do not forget to install selenium if you going to test UI
+//  http://selenium-release.storage.googleapis.com/index.html
+//  And start it before execution the tests
+//  java -jar selenium-server-standalone-XXXXXX.jar   //  XXXXX - downloaded version
+//
+//  More info about selenium you can find here
+//  https://code.google.com/p/selenium/wiki/DesiredCapabilities
+//
+//  Good article
+//  http://codeception.com/11-12-2013/working-with-phpunit-and-selenium-webdriver.html
+//
+//  More examplesof UI testing with selenium
+//  https://github.com/DavertMik/php-webdriver-demo/blob/master/tests/GitHubTest.php
+//
+// ---------------------------------------------------------------------------------------------------
+
+
+
+
+include_once 'unit_tests_config.php';
+include_once 'vendor/autoload.php';
