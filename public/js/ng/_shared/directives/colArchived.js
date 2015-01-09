8e826d6ff110a04f34ffb84064efdb9ba096b684
Vadym <vadym@agile55.com>
2015-01-09 19:16:44 +0200

    confirm quote deletion, icons

--- public/js/ng/_shared/directives/colArchived.js
+++ public/js/ng/_shared/directives/colArchived.js
@@ -17,9 +17,9 @@ app_module.directive('colArchived', ['$compile', function($compile){
 
                     scope.$watch('colArchivedWatch', function(newVal, oldVal){
                         if (element.attr('data') == 1) {
-                            var html = '<span class="icon-archive">Archived. Click to unarchive.</span>';
+                            var html = '<span class="icon-archive"> Archived. Click to unarchive.</span>';
                         } else {
-                            var html = '<span class="icon-down-dir">Click to move to archive.</span>';
+                            var html = '<span class="icon-down-dir"> Click to move to archive.</span>';
                         }
                         var template = angular.element(html);
                         var linkFn = $compile(template);
